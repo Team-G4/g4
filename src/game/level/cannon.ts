05bd51a65347c2567a6670823d90117dc0619f2c
scintilla4evr <kacper17@outlook.com>
2020-02-04 05:06:27 +0100

    why are the bullets so slow

--- src/game/level/cannon.ts
+++ src/game/level/cannon.ts
@@ -84,8 +84,8 @@ export class Cannon implements IPrimitive {
         return new Bullet(
             x + 20 * Math.cos(angle),
             y + 20 * Math.sin(angle),
-            1500 * Math.cos(angle),
-            1500 * Math.sin(angle),
+            2000 * Math.cos(angle),
+            2000 * Math.sin(angle),
             this
         )
     }
