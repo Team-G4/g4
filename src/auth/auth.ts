2126445d10c2c3953bff1acb89b2584001306d1c
scintilla4evr <kacper17@outlook.com>
2020-02-05 03:29:44 +0100

    Started working on bringing G4 auth

---
+++ src/auth/auth.ts
@@ -0,0 +1,34 @@
+import { EventEmitter } from "../util/events"
+
+let apiEndpoint = "https://g4-leaderboard.herokuapp.com"
+
+export class Account extends EventEmitter {
+    private userUUID: string
+    private accessToken: string
+    public userName: string
+
+    async request(endpoint: string, body: any): Promise<Response> {
+        return fetch(
+            apiEndpoint + endpoint,
+            {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            }
+        )
+    }
+
+    async logout() {
+        await this.request(
+            "/userLogout",
+            { uuid: this.userUUID }
+        )
+
+        this.userUUID = null
+        this.userName = null
+        this.accessToken = null
+    }
+}
