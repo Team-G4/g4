02aa9699df58d9c95bc663c14bab7492061e636e
scintilla4evr <kacper17@outlook.com>
2020-03-12 21:13:38 +0100

    Added a loading screen

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { WebG4 } from "./g4"
 
-import WebMidi, { Input } from "webmidi"
-
 import "./main.scss"
-import { LocalStorageSettingsManager } from "./settings/localStorage"
 
 const g4 = new WebG4()
 
 g4.preload().then(() => {
-    g4.start();
+    document.body.classList.add("loaded");
+
+    document.querySelector("button.startGame").addEventListener("click", () => {
+        (document.querySelector("div.loadingScreen") as HTMLDivElement).style.display = "none"
+        g4.start()
+    });
     
     // Just for playing around
     (window as any).setThemeColor = (color: string, value: string) => {
